50c180b50a868f6c8b90ca609945edc5c9750586
IwonaWilk <iwona.k.bialecka@gmail.com>
2020-04-13 13:17:08 -0500

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  def default_url_options
+    { host: ENV['DOMAIN'] || 'localhost:3000' }
+  end
 end
