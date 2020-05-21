c6727b03b14e4fd2c3cb5594fc17fcad24591188
Iwona Wilk <iwona.k.bialecka@gmail.com>
2020-05-20 20:51:18 -0500

    logging every request and response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
+require 'json'
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  around_action :logging_traffic
 
   def default_url_options
     { host: ENV['DOMAIN'] || 'localhost:3000' }
+  end
+
+  def logging_traffic
+    puts "USERAGENT: #{request.original_url}, #{request.method}"
+    begin
+      yield
+    ensure
+      puts "response_status: #{response.status}"
+    end
   end
 end
