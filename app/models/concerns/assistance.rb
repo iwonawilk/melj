9ca6aa9dba502d453830b12994d45e8807b9cdb5
Iwona Wilk <iwona.k.bialecka@gmail.com>
2020-12-15 14:03:15 -0600

    Extra question and validation for a phone number - financial assistance form

--- app/models/concerns/assistance.rb
+++ app/models/concerns/assistance.rb
@@ -1,7 +1,7 @@
 class Assistance < MailForm::Base
   attribute :name, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :phone, validate: true
+  attribute :phone, validate: /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/i
   attribute :alternatecontact, validate: true
   attribute :address, validate: true
   attribute :county, validate: true
@@ -10,12 +10,13 @@ class Assistance < MailForm::Base
   attribute :incarcerated, validate: true
   attribute :program, validate: true
   attribute :helpwith, validate: true
+  attribute :money, validate: true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      subject: 'Assistance Form Inquiry',
+      subject: 'Financial Assistance Form Inquiry',
       to: 'meljcenter@gmail.com',
       from: %("#{name}" <#{email}>)
     }
